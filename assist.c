aee5d14f3abaf1cd69a7009dca1f4af3fe5e5607
markplagge <plaggm@rpi.edu>
2015-07-14 08:53:31 -0400

    removed traling s

--- assist.c
+++ assist.c
@@ -41,7 +41,7 @@ tw_stime getNextEventTime(tw_lp *lp) {
   // tw_rand_binomial(lp->rng, 100, CLOCK_RANDOM_ADJ);
     r = (double)tw_rand_integer(lp->rng, INT32_MIN, INT32_MAX);
           r = r / 100000000000;
-          r += lp->gid / 100000000000;s
+          r += lp->gid / 100000000000;
           break;
 
   }
