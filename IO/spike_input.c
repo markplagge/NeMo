a796b54f1397520c356517e2c8c784d8b7b08a74
Mark Plagge <plaggm@rpi.edu>
2016-04-05 15:14:07 -0400

    Started up IO code. Cleaning up system for CR.

---
+++ IO/spike_input.c
@@ -0,0 +1,9 @@
+//
+//  spike_input.c
+//  ROSS_TOP
+//
+//  Created by Mark Plagge on 4/5/16.
+//
+//
+
+#include "spike_input.h"
---
+++ IO/spike_input.h
@@ -0,0 +1,14 @@
+//
+//  spike_input.h
+//  ROSS_TOP
+//
+//  Created by Mark Plagge on 4/5/16.
+//
+//
+
+#ifndef spike_input_h
+#define spike_input_h
+
+#include <stdio.h>
+
+#endif /* spike_input_h */
