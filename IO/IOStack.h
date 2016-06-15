7ddddd02967073c9745b7fcabeb4471e2f31d796
Mark Plagge <plaggm@rpi.edu>
2016-06-15 17:37:56 -0400

    added basic IO stack elements

--- IO/IOStack.h
+++ IO/IOStack.h
@@ -5,4 +5,37 @@
 #ifndef NEMO_IOSTACK_H
 #define NEMO_IOSTACK_H
 
+/**
+ * { item_description }
+ */
+typedef struct CSV_Element {
+	char * data;
+	void * next;
+}csvElement;
+
+/**
+ * { item_description }
+ */
+typedef struct CSV_Header {
+	char * header;
+	csvElement data;
+}csvHeader;
+/**
+ * { item_description }
+ */
+typedef struct CSV_DATA{ 
+	char * filename;
+	char * metaFileName;
+	char * metadata;
+
+	csvHeader data[];
+
+}csvData;
+
+void initFiles();
+void closeFiles();
+
+void writeData(csvData *data);
+
+
 #endif //NEMO_IOSTACK_H
