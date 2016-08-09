96e8d326115231ad85829e659ca76775113bbe3c
Neil McGlohon <nmcglohon@gmail.com>
2016-08-09 15:49:15 -0400

    Neuron File Creation

---
+++ neuro/lif_neuron.c
@@ -0,0 +1,5 @@
+//
+// Created by Neil McGlohon on 8/9/16.
+//
+
+#include "lif_neuron.h"
---
+++ neuro/lif_neuron.h
@@ -0,0 +1,30 @@
+//
+// Created by Mark Plagge on 5/25/16.
+//
+
+#ifndef __NEMO_LIF_NEURON_H__
+#define __NEMO_LIF_NEURON_H__
+
+
+
+#include "../globals.h"
+#include "../mapping.h"
+#include "../IO/IOStack.h"
+#include <math.h>
+#define Vj ns->membranePotential
+
+#ifdef SAVE_MSGS
+
+
+
+
+#endif
+
+#ifdef SAVE_NEURON_STATS
+
+
+
+#endif
+
+
+#endif //NEMO_LIF_NEURON_H
