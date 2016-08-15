258021dac54febe8fe2a7ca8702a208dadccf11e
Neil McGlohon <nmcglohon@gmail.com>
2016-08-15 18:43:38 -0400

    LIF Neuron Header Initialization

--- neuro/lif_neuron.h
+++ neuro/lif_neuron.h
@@ -13,18 +13,135 @@
 #include <math.h>
 #define Vj ns->membranePotential
 
-#ifdef SAVE_MSGS
+
+
+
+typedef struct LIF_MODEL
+{
+     //64
+     tw_stime lastActiveTime; /**< last time the neuron fired - used for calculating leak and reverse functions. Should be a whole number (or very close) since big-ticks happen on whole numbers. */
+     tw_stime lastLeakTime;/**< Timestamp for leak functions. Should be a mostly whole number, since this happens once per big tick. */
+
+     tw_lpid outputGID; //!< The output GID (axon global ID) of this neuron.
+
+     stat_type rcvdMsgCount; //!<  The number of synaptic messages received.
+     stat_type SOPSCount; //!<  A count for SOPS calculation
+
+
+     //32
+     volt_type membranePotential; //!< current "voltage" of neuron, \f$V_j(t)\f$. Since this is PDES, \a t is implicit
+     thresh_type posThreshold; //!< neuron's threshold value 𝛼
+     thresh_type negThreshold; //!< neuron's negative threshold, 𝛽
+
+
+     //16
+     id_type dendriteLocal; //!< Local ID of the remote dendrite -- not LPID, but a local axon value (0-i)
+     random_type drawnRandomNumber; //!<When activated, neurons draw a new random number. Reset after every big-tick as needed.
+     random_type thresholdPRNMask;/**!< The neuron's random threshold mask - used for randomized thresholds ( \f$M_j\f$ ).
+                                *	In the TN hardware this is defined as a ones maks of configurable width, starting at the
+                                * least significant bit. The mask scales the range of the random drawn number (PRN) of the model,
+                                * here defined as @link drawnRandomNumber @endlink. used as a scale for the random values. */
+
+     id_type myCoreID; //!< Neuron's coreID
+
+     id_type myLocalID;//!< my local ID - core wise. In a 512 size core, neuron 0 would have a local id of 262,657.
+
+
+     //small
+     short largestRandomValue;
+     short lambda; //!< leak weight - \f$𝜆\f$ Leak tuning parameter - the leak rate applied to the current leak function.
+     short int resetMode; //!< Reset mode selection. Valid options are 0,1,2 . Gamma or resetMode 𝛾
+     volt_type resetVoltage; //!< Reset voltage for reset params, \f$R\f$.
+     short sigmaVR; //!< reset voltage - reset voltage sign
+     short encodedResetVoltage; //!< encoded reset voltage - VR.
+     short omega; //!<temporary leak direction variable
+
+     char* neuronTypeDesc; //!< a debug tool, contains a text desc of the neuron.
+     char sigma_l; //!< leak sign bit - eqiv. to σ
+     unsigned char delayVal; //!<@todo: Need to fully implement this - this value is between 1 and 15, a "delay" of n timesteps of a neuron. -- outgoing delay //from BOOTCAMP!
+
+     //@TODO - convert this to a bitfield for bools. Check ROSS BF implementation
+
+     bool firedLast;
+     bool heartbeatOut;
+     bool isSelfFiring;
+     bool epsilon; //!<epsilon function - leak reversal flag. from the paper this changes the function of the leak from always directly being integrated (false), or having the leak directly integrated when membrane potential is above zero, and the sign is reversed when the membrane potential is below zero.
+     bool c; //!< leak weight selection. If true, this is a stochastic leak function and the \a leakRateProb value is a probability, otherwise it is a leak rate.
+     bool kappa; //!<Kappa or negative reset mode. From the paper's ,\f$𝜅_j\f$, negative threshold setting to reset or saturate
+     bool canGenerateSpontaniousSpikes;
 
 
 
 
-#endif
+     char axonTypes[512];
+     char synapticWeight[4];
+     bool synapticConnectivity[512]; //!< is there a connection between axon i and neuron j?
+     /** stochastic weight mode selection. $b_j^{G_i}$ */
+     bool weightSelection[4];
 
-#ifdef SAVE_NEURON_STATS
 
 
 
-#endif
+}lif_neuron_state
+
+
+
+
+
+/**
+ * @brief      Leaky Integrate and Fire Forward Event handler
+ *
+ * @param      s                The lif neuron state
+ * @param      CV               flags for message flow
+ * @param      messageData      The message data
+ * @param      lp               The pointer to a LP
+ */
+void LIF_forward_event (lif_neuron_state *s, tw_bf *CV, messageData *m,
+    tw_lp *lp);
+
+/**
+ * @brief      Leaky Integrate and Fire Reverse Event Handler
+ *
+ * @param      s                The lif neuron state
+ * @param      CV               flags for message flow
+ * @param      messageData      The message data
+ * @param      lp               The pointer to a
+ */
+void LIF_reverse_event (lif_neuron_state *s, tw_bf *CV, messageData *m,
+    tw_lp *lp);
+
+
+void LIF_commit(lif_neuron_state *s, tw_bf * cv, messageData *m, tw_lp *lp);
+
+/**
+ * @brief      Initialize a Leaky Integrate and Fire neuron
+ *
+ * @param      s     The lif neuron state
+ * @param      lp    The pointer to the LP
+ */
+void LIF_init(lif_neuron_state *s, tw_lp *lp);
+
+/**
+ * @brief      The Leaky Integrate and Fire neuron final function
+ *
+ * @param      s     The lif neuron State
+ * @param      lp    The pointer to an LP
+ */
+void LIF_final(lif_neuron_state *s, tw_lp *lp);
+
+
+/**
+ * @brief	This takes a void pointer and returns this neuron's struct.
+ * This is used for managing super synapse direct communication functionality.
+ */
+
+inline lif_neuron_state * TN_convert(void * lpstate);
+
+
+
+
+
+
 
 
 #endif //NEMO_LIF_NEURON_H
