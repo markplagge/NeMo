5cd0e2283857e3ceea8d5c72726dc68d7ff801e7
Neil McGlohon <nmcglohon@gmail.com>
2016-08-16 18:23:23 -0400

    small declarations

--- neuro/lif_neuron.c
+++ neuro/lif_neuron.c
@@ -3,3 +3,22 @@
 //
 
 #include "lif_neuron.h"
+
+
+void LIFFire(tn_neuron_state *st, void *l);
+
+void LIFIntegrate(id_type snyapseID, lif_neuron_state *st, void *lp);
+
+bool LIFReceiveMessage(lif_neuron_state *st, messageData *mes, tw_lp *lp, tw_bf *bf);
+
+void LIFReceiveReverseMessage(lif_neuron_state *st, messageData *mes, tw_lp *lp, tw_bf *bf);
+
+void LIFShouldFire(lif_neuron_state *st, tw_lp *lp);
+
+bool LIFFireFloorCeilingReset(lif_neuron_state *st, tw_lp *lp);
+
+void LIFNumericLeakCalc(lif_neuron_state *st, tw_stime now);
+
+void LIFLinearLeak(lif_neuron_state *st, tw_stime now);
+
+void LIFSendHeartbeat(lif_neuron_state *st tw_stime time, void *lp);
