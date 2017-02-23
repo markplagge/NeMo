f686244e7b1e3928046007e5f6bd1a3d6f6e4779
Mark Plagge <plaggm@rpi.edu>
2017-02-23 17:41:44 -0500

    input scaffold 1

--- src/IO/input.c
+++ src/IO/input.c
@@ -6,7 +6,7 @@
 
 static char* networkTempName;
 static FILE* networkTempFile;
-
+static FILE* networkTempFiles[];
 int openInputFiles(){
     networkFile = fopen(networkFileName,"r");
     if (errno){
@@ -15,10 +15,16 @@ int openInputFiles(){
         return errno;
     }
 
-    networkTempName = calloc(64, sizeof(char));
-    networkTempName = sprintf(networkTempName,"NeMoNetworkTemp_rank%l.dat", g_tw_mynode);
-    networkTempFile = fopen(networkTempName, "wb");
+    int ws = g_tw_npe;
 
+    networkTempFiles = calloc(ws,sizeof(FILE*));
+
+    for (int i = 0; i < ws; i ++) {
+        networkTempName = calloc(64, sizeof(char));
+        networkTempName = sprintf(networkTempName, "NeMoNetworkTemp_rank%l.dat", g_tw_mynode);
+        networkTempFile = fopen(networkTempName, "wb");
+        networkTempFiles[i] = networkTempFile;
+    }
     if (errno){
         printf("Error saving network temp file. Error code %i  \n", errno);
         return errno;
@@ -30,6 +36,27 @@ int openInputFiles(){
 
 /** Callback function called when libCSV has read an entire field */
 void fld_read (void *s, size_t len, void *data){
+    //When a field is read, the data in string format is given here.
+    //assemble a complete data structure based on the type (use state machine st 1)
+    //cast the values before storing in the struct.
+}
+void line_read(void *s, size_t len, void *data) {
+    //once a line has been read in, look at the global struct where data is stored.
+    //convert the csv struct into a neuron state struct.
+    //next, save the neuron state struct into a binary file (append)
+    //pick the binary file based on which rank the NS core should run on.
 
 }
+void parseNetworkFile(){
+    //Only run on MPI first rank
+    //OR
+    //Run in parallel, ignoring lines that have a different core than what this
+    //process' rank would be running.
+    //read CSV file line by line.
+    //Parse the simulation parameters, and store them
+    //Once in neuron defs, start using CSV_PARSE with the fld_read callback until done.
+}
 
+void readNeurons(){
+
+}
