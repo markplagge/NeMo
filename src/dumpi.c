b5dd671e4e0a53b15e8e8ae4b13e7c0d87e861fb
Mark Plagge <plaggm@rpi.edu>
2017-08-19 10:36:55 -0400

    Added dumpi template file skeleton

---
+++ src/dumpi.c
@@ -0,0 +1,5 @@
+//
+// Created by Mark Plagge on 8/18/17.
+//
+
+#include "dumpi.h"
---
+++ src/dumpi.h
@@ -0,0 +1,8 @@
+//
+// Created by Mark Plagge on 8/18/17.
+//
+
+#ifndef SUPERNEMO_DUMPI_H
+#define SUPERNEMO_DUMPI_H
+
+#endif //SUPERNEMO_DUMPI_H
