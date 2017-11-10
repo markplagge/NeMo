31ce7e74e3604224823a4acd28ea46caf74ecb83
Mark Plagge <plaggm@rpi.edu>
2017-11-10 18:54:04 -0500

    updated to lua 5.3 for config file

--- scripts/read_tn_json.py
+++ scripts/read_tn_json.py
@@ -1,46 +1,52 @@
 import click
 import os
 from tn_json_readers import createNeMoCFGFromJson, readAndSaveSpikeFile
+import shutil
 
 
 class nOpts:
-	def __init__(self, csvFN):
-		self.csvFN = csvFN
+    def __init__(self, csvFN):
+        self.csvFN = csvFN
 
 
 @click.command()
 @click.option('--modelname', default='nemo_', help="The file prefix used for this model/spike file parse run")
-#@click.option('--cores', default=1024, help="The number of NS cores to simulate NOT IMPLEMENTED")
+# @click.option('--cores', default=1024, help="The number of NS cores to simulate NOT IMPLEMENTED")
 @click.option('--jmodel', help="The TN JSON file to read")
 @click.option('--jspike', help="The TN JSON spike file to read")
-def cli(modelname,jmodel,jspike):
-	ctx  = nOpts(modelname)
+def cli(modelname, jmodel, jspike):
+    ctx = nOpts(modelname)
 
-	ctx.jspike = jspike
-	ctx.jmodel = jmodel
-	readJSON(ctx)
-
+    ctx.jspike = jspike
+    ctx.jmodel = jmodel
+    readJSON(ctx)
 
 
 def readJSON(ctx):
-	outPrefix = ctx.csvFN
-	modelFile = ctx.jmodel
-	spikeFile = ctx.jspike
+    outPrefix = ctx.csvFN
+    modelFile = ctx.jmodel
+    spikeFile = ctx.jspike
 
-	click.echo("Starting JSON read - will take some time.")
-	df = createNeMoCFGFromJson(modelFile,outPrefix+"_model")
-	df.closeFile()
-	with open(df.destination, 'r') as f:
-		fdat = f.readline()
+    click.echo("Starting JSON read - will take some time.")
+    df = createNeMoCFGFromJson(modelFile, outPrefix + "_model")
+    df.closeFile()
 
-	with open(df.destination, 'w') as f:
-		f.writelines(fdat[2:])
+    # remove the first two lines per new spec:
+    with open(df.destination, 'r') as source_file:
+        source_file.readline()
+        source_file.readline()
+        with open('trimmed_' + df.destination, 'w') as target_file:
+            shutil.copyfileobj(source_file, target_file)
 
-	readAndSaveSpikeFile(filename=spikeFile, saveFile=outPrefix+"_spike.csv")
 
+    #with open(df.destination, 'r') as f:
+    #    fdat = f.readline()
+    #fdat = f.readlines()
+    #with open("trimed_" + df.destination, 'w') as f:
+    #    f.writelines(fdat[2:])
+
+    readAndSaveSpikeFile(filename=spikeFile, saveFile=outPrefix + "_spike.csv")
 
 
 if __name__ == '__main__':
-	cli()
-
-
+    cli()
