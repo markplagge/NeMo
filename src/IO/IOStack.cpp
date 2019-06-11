75a050d3a52c38342acf78285fd5c7ce5ce3b846
Mark Plagge <plaggm@rpi.edu>
2019-06-11 07:08:30 -0600

    changed IO stack to C++ for easy file IO

---
+++ src/IO/IOStack.cpp
@@ -0,0 +1,44 @@
+//
+// Created by Mark Plagge on 5/25/16.
+//
+
+#include "IOStack.h"
+#include "../nemo_config.h"
+
+FILE *weightsFP;
+FILE *dataFP;
+void prGetWeights(int *weights, long long coreID, long long neuronID){
+    static int isWOpen = 0;
+    if (!isWOpen) {
+        weightsFP = fopen(PR_WEIGHT_FILE, "r");
+        isWOpen = 1;
+    }
+    //get the data from the file
+    
+}
+FILE * inputFile;
+bool inputFileOpen;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void saveEvent(tw_stime timestamp, char sourceType, id_type core, id_type local,
+               id_type destCore, id_type destLocal){
+	
+
+	
+}
+
